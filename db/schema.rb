adb104d0b08610043b9f7bc96354c4af7239d772
Francesco Lupano <lupano.web90@gmail.com>
2022-04-21 17:31:39 +0200

    added tailwind and basic routs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_05_100544) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_21_140740) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -21,6 +21,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_05_100544) do
     t.datetime "updated_at", null: false
     t.string "provider"
     t.string "uid"
+    t.string "nickname"
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
