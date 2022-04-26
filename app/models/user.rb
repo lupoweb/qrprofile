5efb43ff0d88ced35950b8ffdce66613720d44d7
Francesco Lupano <lupano.web90@gmail.com>
2022-04-26 10:17:07 +0200

    create qr code svg

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,7 @@ class User < ApplicationRecord
       user.email = auth.info.email
       user.name = auth.info.name
       user.nickname = auth.info.nickname
+      user.image_url = auth['info']['image']
       user.password = Devise.friendly_token[0,20]
     end
   end
